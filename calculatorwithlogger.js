74af25061ef5d7794c271bd3b64e951d33d38024
AnthonyKB <akbea@deakin.edu.au>
2024-04-06 18:14:18 +1100

    first commit

--- calculatorwithlogger.js
+++ calculatorwithlogger.js
@@ -43,8 +43,8 @@ app.get("/add", (req,res)=>{ //defining a router handler '/add' for HTTP GET req
         throw new Error("n2 incorrectly defined");
     }
     
-    logger.info('Parameters '+n1+' and '+n2+' received for addition'); //Text that will be put into the combined.log document.
-    const result = add(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'add' function in junction with n1 and n2 to create the new value.
+    logger.info('Parameters '+n1+' and '+n2+' received for addition'); //Text that will be put into the combined.log document
+    const result = add(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'add' function in junction with n1 and n2 to create the new value
     res.status(200).json({statuscocde:200, data: result }); //displays result
     } catch(error) { //Error catching
         console.error(error)
@@ -68,8 +68,8 @@ app.get("/subtract", (req,res)=>{ //defining a router handler '/subtract' for HT
       throw new Error("n2 incorrectly defined");
   }
   
-  logger.info('Parameters '+n1+' and '+n2+' received for subtraction'); //Text that will be put into the combined.log document.
-  const result = subtract(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'subtract' function in junction with n1 and n2 to create the new value.
+  logger.info('Parameters '+n1+' and '+n2+' received for subtraction'); //Text that will be put into the combined.log document
+  const result = subtract(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'subtract' function in junction with n1 and n2 to create the new value
   res.status(200).json({statuscocde:200, data: result }); //displays the result
   } catch(error) { //Error catching
       console.error(error)
@@ -93,8 +93,8 @@ app.get("/divide", (req,res)=>{ //defining a router handler '/divide' for HTTP G
         throw new Error("n2 incorrectly defined");
     }
     
-    logger.info('Parameters '+n1+' and '+n2+' received for division'); //Text that will be put into the combined.log document.
-    const result = divide(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'divide' function in junction with n1 and n2 to create the new value.
+    logger.info('Parameters '+n1+' and '+n2+' received for division'); //Text that will be put into the combined.log document
+    const result = divide(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'divide' function in junction with n1 and n2 to create the new value
     res.status(200).json({statuscocde:200, data: result }); //displays the result
     } catch(error) { //Error catching
         console.error(error)
@@ -118,8 +118,86 @@ app.get("/multiply", (req,res)=>{ //defining a router handler '/multiply' for HT
       throw new Error("n2 incorrectly defined");
   }
   
-  logger.info('Parameters '+n1+' and '+n2+' received for multiplication'); //Text that will be put into the combined.log document.
-  const result = multiply(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'multiply' function in junction with n1 and n2 to create the new value.
+  logger.info('Parameters '+n1+' and '+n2+' received for multiplication'); //Text that will be put into the combined.log document
+  const result = multiply(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'multiply' function in junction with n1 and n2 to create the new value
+  res.status(200).json({statuscocde:200, data: result }); //displays the result
+  } catch(error) { //Error catching
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+
+//TASK 4.2C OPERATIONS
+
+const power= (n1,n2) => { //n1 and n2 being the two values used for the calculation
+  return Math.pow(n1, n2);             //This is the equation that gives us the exponent value using n1 and n2 values
+}
+app.get("/power", (req,res)=>{ //defining a router handler '/power' for HTTP GET requests
+  try{
+  const n1= parseFloat(req.query.n1); //converting the n1 and n2 values from a string into a number
+  const n2= parseFloat(req.query.n2);
+  if(isNaN(n1)) { //Error thrown "n1 is incorrectly defined" when n1 is not defined
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+  if(isNaN(n2)) { //Error thrown "n2 is incorrectly defined" when n2 is not defined
+      logger.error("n2 is incorrectly defined");
+      throw new Error("n2 incorrectly defined");
+  }
+  
+  logger.info('Parameters '+n1+' and '+n2+' received for exponentiation'); //Text that will be put into the combined.log document
+  const result = power(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'power' function in junction with n1 and n2 to create the new value
+  res.status(200).json({statuscocde:200, data: result }); //displays the result
+  } catch(error) { //Error catching
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+
+//Square root function will only need n1 value to be used, as it will get the square root of a single value
+//Entering an n2 value will be completely ignored, and only n1 value will be used for the calculation
+
+const root= (n1) => { //n1 and n2 being the two values used for the calculation
+  return Math.sqrt(n1);             //This is the equation that gives us the square root value using n1 value only
+}
+app.get("/root", (req,res)=>{ //defining a router handler '/root' for HTTP GET requests
+  try{
+  const n1= parseFloat(req.query.n1); //converting the n1 and n2 values from a string into a number
+
+  if(isNaN(n1)) { //Error thrown "n1 is incorrectly defined" when n1 is not defined
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+  
+  logger.info('Parameters '+n1+' received for square root'); //Text that will be put into the combined.log document
+  const result = root(n1); //Creating 'result' which will be used later, it uses preexisting 'root' function in junction with n1 to create the new value
+  res.status(200).json({statuscocde:200, data: result }); //displays the result
+  } catch(error) { //Error catching
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+const mod= (n1,n2) => { //n1 and n2 being the two values used for the calculation
+  return n1 % n2;            //This is the equation that gives us the modulo value
+}
+app.get("/mod", (req,res)=>{ //defining a router handler '/mod' for HTTP GET requests
+  try{
+  const n1= parseFloat(req.query.n1); //converting the n1 and n2 values from a string into a number
+  const n2= parseFloat(req.query.n2);
+  if(isNaN(n1)) { //Error thrown "n1 is incorrectly defined" when n1 is not defined
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+  if(isNaN(n2)) { //Error thrown "n2 is incorrectly defined" when n2 is not defined
+      logger.error("n2 is incorrectly defined");
+      throw new Error("n2 incorrectly defined");
+  }
+  
+  logger.info('Parameters '+n1+' and '+n2+' received for modulo'); //Text that will be put into the combined.log document
+  const result = mod(n1,n2); //Creating 'result' which will be used later, it uses preexisting 'mod' function in junction with n1 and n2 to create the new value
   res.status(200).json({statuscocde:200, data: result }); //displays the result
   } catch(error) { //Error catching
       console.error(error)
@@ -129,7 +207,7 @@ app.get("/multiply", (req,res)=>{ //defining a router handler '/multiply' for HT
 
 
 
-const port=3040; //Setting the port to 3040, this is the port we will use to view the calculations.
+const port=3040; //Setting the port to 3040, this is the port we will use to view the calculations
 app.listen(port,()=> { //Telling the app to start listening to the port, which is 3040
     console.log("hello i'm listening to port"+port); 
 })
